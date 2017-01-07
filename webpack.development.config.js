2b7317b196544f80a2c99aaa16ef6117da8adbbf
AbsoluteZero273 <chaudharypratik97@gmail.com>
2017-01-07 16:42:01 +0545

    Fix indentations

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -13,7 +13,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-      new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     loaders: [{
