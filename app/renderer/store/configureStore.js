3037c36d1bb9b5a90cd3eb07ed92f2551ce2cb4d
AbsoluteZero273 <chaudharypratik97@gmail.com>
2017-01-20 19:56:27 +0545

    Use cached version of process object

--- app/renderer/store/configureStore.js
+++ app/renderer/store/configureStore.js
@@ -6,7 +6,7 @@ import { createStore } from 'redux';
 // import { combineReducers } from 'react-redux';
 
 export default () => {
-  if (remote.getGlobal('process').env.NODE_ENV === 'development') {
+  if (remote.process.env.NODE_ENV === 'development') {
     return createStore(isPlaying,
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
   } else {
