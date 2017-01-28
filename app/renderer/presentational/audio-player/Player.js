9697b3564dc2b3b388ddabf5a6a55ea628a744ba
AbsoluteZero273 <chaudharypratik97@gmail.com>
2017-01-28 19:36:05 +0545

    Extract humanizeSeconds

--- app/renderer/presentational/audio-player/Player.js
+++ app/renderer/presentational/audio-player/Player.js
@@ -3,6 +3,7 @@ import defaultAlbumImage from '../../../assets/images/album-art-default.png';
 import Audio from './Audio';
 import Slider from './../generic/Slider';
 import Volume from './Volume';
+import { humanizeSeconds } from './../../utils/time';
 
 const Player = ({
   playFromSeconds,
@@ -59,26 +60,6 @@ const Player = ({
         </div>
     </section>
   );
-};
-
-const humanizeSeconds = (second) => {
-  let time = Math.trunc(second);
-  let sec = time % 60;
-  time = Math.trunc(time / 60);
-  let min = time % 60;
-  time = Math.trunc(time / 60);
-  let hour = time % 24;
-
-  let humanizedTime = '';
-  if (hour) humanizedTime += (oneDigitNumber(hour) ? `0${hour}` : hour) + ':';
-  humanizedTime += (oneDigitNumber(min) ? `0${min}` : min) + ':';
-  humanizedTime += (oneDigitNumber(sec) ? `0${sec}` : sec);
-
-  return humanizedTime;
-};
-
-const oneDigitNumber = (num) => {
-  return num >= 0 && num <=9;
 };
 
 export default Player;
