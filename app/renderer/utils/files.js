9a368dea3717d6dfd63bf0822f1fabde7699ed8c
AbsoluteZero273 <chaudharypratik97@gmail.com>
2017-02-04 15:47:58 +0545

    Rewrite readDirRecursively

--- app/renderer/utils/files.js
+++ app/renderer/utils/files.js
@@ -2,27 +2,38 @@ import fs from 'fs';
 import path from 'path';
 
 export const readDirRecursively = (rootDir) => {
-  const files = [];
-
   return new Promise((resolve, reject) => {
-    // Inner function for recursive operation
-    function innerFun(dir) {
+
+    function innerFun(dir, cb) {
+      let files = [];
       fs.readdir(dir, (err, xpaths) => {
-        if (err) reject(err);
+        if (err) reject(err, null);
+        let filesCount = xpaths.length;
+        if (!filesCount) return cb(files);
 
         for (let xpath of xpaths) {
           const absPath = path.join(dir, xpath);
           if (fs.statSync(absPath).isDirectory()) {
-            innerFun(absPath);
+            innerFun(absPath, (err, xfiles) => {
+              if (err) return (cb(err, null));
+              files = files.concat(xfiles);
+              filesCount--;
+              if (filesCount <= 0) return cb(null, files);
+            });
           } else {
             files.push(absPath);
+            filesCount--;
+            if (filesCount <= 0) {
+              return cb(null, files);
+            }
           }
         }
       });
     }
 
-    innerFun(rootDir);  // Call innerFun to initate
-    resolve(files);
+    innerFun(rootDir, (err, files) => {
+      if (err) reject(err);
+      resolve(files);
+    });
   });
-
 };
