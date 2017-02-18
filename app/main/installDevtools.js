2eab157ebe0d8cdfa7943742dc621161e869ac1e
AbsoluteZero273 <chaudharypratik97@gmail.com>
2017-02-18 14:46:30 +0545

    Remove unnecessary comments

--- app/main/installDevtools.js
+++ app/main/installDevtools.js
@@ -6,7 +6,6 @@ module.exports = () => {
       REDUX_DEVTOOLS
     } = require('electron-devtools-installer')
 
-    /* eslint-disable no-console */
     installExtension(REACT_DEVELOPER_TOOLS)
       .then((name) => console.log(`Added Extension:  ${name}`))
       .catch((err) => console.log('An error occurred: ', err))
@@ -14,6 +13,5 @@ module.exports = () => {
     installExtension(REDUX_DEVTOOLS)
       .then((name) => console.log(`Added Extension:  ${name}`))
       .catch((err) => console.log('An error occurred: ', err))
-    /* eslint-enable no-console */
   }
 }
