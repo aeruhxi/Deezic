ad44af00ab5f38929bdd835eb685e4c32a02459f
AbsoluteZero273 <chaudharypratik97@gmail.com>
2017-03-10 14:26:02 +0545

    Make Table component more powerful

--- app/renderer/presentational/generic/Table.js
+++ app/renderer/presentational/generic/Table.js
@@ -1,31 +1,45 @@
-import React from 'react'
+/**
+ * Currently not in use
+ */
+import React, { Component } from 'react'
 
 const NON_NUMERIC_CLASS = 'mdl-data-table__cell--non-numeric'
 
-const Table = ({ headers, rows, className, indexed, indexColumnName }) => {
-  return (
-    <table className={`mdl-data-table mdl-js-data-table  mdl-shadow--2dp ${className}`}>
-      <thead>
-        <tr>
-          {indexed && <th key='id' className={NON_NUMERIC_CLASS}>{indexColumnName}</th>}
-          {headers.map((header) => (
-            <th key={header.name} className={NON_NUMERIC_CLASS}>{header.name}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map((row, index) => (
-          <tr key={row.id || index}>
-            {indexed && <td key='id' className={NON_NUMERIC_CLASS}>{index + 1}</td>}
-            {Object.keys(row).map((columnName, id) => {
-              if (columnName === 'id') return
-              return <td key={id} className={NON_NUMERIC_CLASS}>{row[columnName]}</td>
-            })}
+class Table extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      hoveredIndex: null
+    }
+  }
+
+  render () {
+    return (
+      <table className={`mdl-data-table mdl-js-data-table  mdl-shadow--2dp ${this.props.className}`}>
+        <thead>
+          <tr>
+            {this.props.indexColumnName && <th key='id' className={NON_NUMERIC_CLASS}>{this.props.indexColumnName}</th>}
+            {this.props.headers.map((header) => (
+              <th key={header.name} className={NON_NUMERIC_CLASS}>{header.name}</th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
-  )
+        </thead>
+        <tbody>
+          {this.renderRow()}
+        </tbody>
+      </table>
+    )
+  }
+
+  renderRow () {
+    return this.props.rows.map((row, index) => {
+      const _row = this.props.renderRow(row, this.state.hoveredIndex === index, index)
+      return React.cloneElement(_row, {
+        onMouseEnter: () => { this.setState({ hoveredIndex: index }) },
+        onMouseLeave: () => { this.setState({ hoveredIndex: null }) }
+      })
+    })
+  }
 }
 
 export default Table
