56bef7bd0d8d66cfccdc5dcd390f040e739b4f9d
AbsoluteZero273 <chaudharypratik97@gmail.com>
2017-03-10 14:31:37 +0545

    Change table to list

--- app/renderer/containers/library/TracksContainer.js
+++ app/renderer/containers/library/TracksContainer.js
@@ -1,26 +1,60 @@
 import { connect } from 'react-redux'
 import Tracks from './../../presentational/pages/library/Tracks'
 import { humanizeSeconds } from './../../utils/time'
+import { togglePlay, setCurrentSong } from './../../actions/player'
+import { allTracks } from '../../reducers/library/tracks'
 
-const mapStateToProps = ({ library }) => {
-  const tracks = library.tracks
+const mapStateToProps = (state) => {
+  const rawTracks = state.library.tracks
+  const tracks = allTracks(rawTracks)
   return {
-    tracks: getTracks(tracks)
+    rawTracks,
+    tracks: getUITracks(tracks),
+    currentSongId: state.player.currentSong.id,
+    isPlaying: state.player.isPlaying
   }
 }
 
-const getTracks = (tracks) => {
+const mergeProps = ({tracks, currentSongId, isPlaying, rawTracks}, { dispatch }) => ({
+  tracks,
+  currentSongId,
+  isPlaying,
+  handlePlayPause: (id) => {
+    if (currentSongId === id) {
+      dispatch(togglePlay())
+    } else {
+      dispatch(setCurrentSong(getSongById(id, rawTracks.byId)))
+      setTimeout(() => dispatch(togglePlay()), 400)
+    }
+  }
+})
+
+const TracksContainer = connect(
+  mapStateToProps,
+  null,
+  mergeProps
+)(Tracks)
+
+export default TracksContainer
+
+const getUITracks = (tracks) => {
   return tracks.map(track => ({
     id: track.id,
     song: track.title,
     artist: track.artist,
-    album: track.album,
+    album: track.album.album,
     time: humanizeSeconds(track.length)
   }))
 }
 
-const TracksContainer = connect(
-  mapStateToProps
-)(Tracks)
-
-export default TracksContainer
+const getSongById = (id, tracksById) => {
+  const track = tracksById[id]
+  return {
+    id: track.id,
+    title: track.title,
+    artist: track.artist,
+    album: track.album.album,
+    albumArt: track.albumArt,
+    src: track.src
+  }
+}
--- app/renderer/presentational/pages/library/Tracks.js
+++ app/renderer/presentational/pages/library/Tracks.js
@@ -1,23 +1,69 @@
 import React, { Component } from 'react'
-import Table from './../../generic/Table'
+import './../../../../assets/styles/pages/library/tracks.css'
 
 class Tracks extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      hoveredTrackID: null
+    }
+    this.handleMouseEnter = this.handleMouseEnter.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+    this.listItemStyle = {
+      background: 'rgba(0, 0, 0, 0.3)'
+    }
+  }
 
   render () {
     return (
-      <Table
-        indexed
-        indexColumnName='SN'
-        className='track-table'
-        headers={[
-          {name: 'Song'},
-          {name: 'Artist'},
-          {name: 'Album'},
-          {name: 'Time'}
-        ]}
-        rows={this.props.tracks}
-        />
+      <ul className='track-list'>
+        {this.props.tracks.map((track, index) => (
+          <li
+            style={this.props.currentSongId === track.id ? this.listItemStyle : null}
+            onMouseEnter={() => this.handleMouseEnter(track.id)}
+            onMouseLeave={this.handleMouseLeave}
+            key={track.id} className='track-item'
+          >
+            <p className='track-item__number'>
+              {this.renderNumber(track, index)}
+            </p>
+            <div className='track-item__details'>
+              <h6>{track.song}</h6>
+              <p>{track.artist} <span className='bullet'>&#8226;</span> {track.album}</p>
+            </div>
+            <div className='track-item__right'>
+              <i className='material-icons track-item__favorite-icon'>favorite_border</i>
+              <span className='track-item__duration'>{track.time}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
     )
+  }
+
+  handleMouseEnter (id) {
+    this.setState({ hoveredTrackID: id })
+  }
+  handleMouseLeave () {
+    this.setState({ hoveredTrackID: null })
+  }
+
+  renderNumber (track, index) {
+    const icon = fontName => (
+      <i
+        onClick={() => this.props.handlePlayPause(track.id)}
+        className='material-icons'>
+        {fontName}
+      </i>
+    )
+
+    if (this.props.currentSongId === track.id) {
+      return icon(this.props.isPlaying ? 'pause' : 'play_arrow')
+    } else if (this.state.hoveredTrackID === track.id) {
+      return icon('play_arrow')
+    } else {
+      return index + 1
+    }
   }
 }
 
