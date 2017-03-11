6dc31de133aa3f9a87c94970d83930bdeda57676
AbsoluteZero273 <chaudharypratik97@gmail.com>
2017-03-11 14:58:33 +0545

    Fix bug: nested array was being made in artists and albums state

--- app/renderer/reducers/library/albums.js
+++ app/renderer/reducers/library/albums.js
@@ -6,12 +6,12 @@ const albums = (state = [], action) => {
     case ADD_LIBRARY_TRACKS:
       return [
         ...state,
-        action.tracks
-        .map(track => ({artist: track.artist, album: track.album}))
-        // Remove duplicates
-        .reduce((acc, track) => (_.some(acc, track) ? acc : acc.concat(track)), [])
-        // Filter out albums that are already in state
-        .filter(album => !_.some(state, album))
+        ...action.tracks
+          .map(track => ({artist: track.artist, album: track.album}))
+          // Remove duplicates
+          .reduce((acc, track) => (_.some(acc, track) ? acc : acc.concat(track)), [])
+          // Filter out albums that are already in state
+          .filter(album => !_.some(state, album))
       ]
     default:
       return state
